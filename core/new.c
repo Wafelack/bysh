8840d97862879d6cd05d38ad903269618e6cbb7f
Wafelack <wafelack@protonmail.com>
2020-07-21 15:40:17 +0200

    Securising create function

--- core/new.c
+++ core/new.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "new.h"
 
 void create(char *name)
 {
-    char commande[256] = "";
+    char *commande = malloc((strlen(name) * sizeof(char) + (6 * sizeof(char))));
     sprintf(commande, "mkdir %s", name);
     system(commande);
     printf("\033[0;32m Succesfuly\033[1;37m created empty project in\033[0;31m %s/\033[1;37m", name);
