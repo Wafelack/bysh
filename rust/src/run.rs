fd2e65dfe4ce07c35ee265e179421914b74a345c
Wafelack <wafelack@protonmail.com>
2020-08-08 16:56:38 +0200

    Debugging run function

--- rust/src/run.rs
+++ rust/src/run.rs
@@ -8,11 +8,11 @@ pub fn run() -> std::io::Result<()> {
     let release = ".\\build\\release\\release.exe";
 
     if Path::new(debug).exists() && !Path::new(release).exists() {
-        Command::new(debug).spawn().expect("Cannot run binary");
+        Command::new(debug).status().expect("Cannot run binary");
         fs::remove_file(debug)?;
         Ok(())
     } else if Path::new(release).exists() && !Path::new(debug).exists() {
-        Command::new(release).spawn().expect("Cannot read binary");
+        Command::new(release).status().expect("Cannot read binary");
         fs::remove_file(release)?;
         Ok(())
     } else {
