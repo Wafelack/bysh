2145ef63c38f851bd573b4e94a1c89d1b0406d9e
Wafelack <wafelack@protonmail.com>
2020-08-08 13:02:45 +0200

    Adding version function

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,10 +2,21 @@ use std::env;
 #[allow(unused_imports)]
 use std::process::exit;
 
+mod version;
+use version::{version, Version};
+
 fn main() {
+    let ver = Version {
+        main: 0,
+        discriminator: 1,
+        third: 0,
+    };
     let argv: Vec<String> = env::args().collect();
     if argv.len() < 2 {
         println!("Usage: wanager <command> [OPTIONS]");
         std::process::exit(0);
+    }
+    if argv[1] == "version" {
+        version(ver);
     }
 }
