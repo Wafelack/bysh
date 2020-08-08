73c89052024a24b7a090d8e260df0af696c3e703
Wafelack <wafelack@protonmail.com>
2020-08-08 12:04:45 +0200

    Adding argv length verification

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
+#[allow(unused_imports)]
+use std::process::exit;
 
 fn main() {
     let argv: Vec<String> = env::args().collect();
-    println!("{:?}", argv);
+    if argv.len() < 2 {
+        println!("Usage: wanager <command> [OPTIONS]");
+        std::process::exit(0);
+    }
 }
